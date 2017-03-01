58e656191561ea069e5c4431703dec6e12ad1dc7
aboveyou00 <brandonyoyoslade@gmail.com>
2017-03-01 15:25:30 -0700

    feat(migrations): recreate migration tsconfig.json file each time you migrate

--- src/util/create-migration-directory.ts
+++ src/util/create-migration-directory.ts
@@ -7,16 +7,18 @@ export function createMigrationDirectory() {
     if (!fs.existsSync(dir)) fs.mkdirSync(dir);
     
     let tsconfigPath = path.join(dir, 'tsconfig.json');
-    if (!fs.existsSync(tsconfigPath)) {
-        let tsconfigStream: fs.WriteStream | null = null;
-        try {
-            tsconfigStream = fs.createWriteStream(tsconfigPath);
-            let template = getTemplate('migration-tsconfig.json');
-            tsconfigStream.write(template);
-        }
-        finally {
-            if (tsconfigStream) tsconfigStream.close();
-        }
+    if (fs.existsSync(tsconfigPath)) {
+        fs.unlinkSync(tsconfigPath);
+    }
+    
+    let tsconfigStream: fs.WriteStream | null = null;
+    try {
+        tsconfigStream = fs.createWriteStream(tsconfigPath);
+        let template = getTemplate('migration-tsconfig.json');
+        tsconfigStream.write(template);
+    }
+    finally {
+        if (tsconfigStream) tsconfigStream.close();
     }
     
     return dir;
